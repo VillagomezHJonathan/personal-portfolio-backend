1f065dc6e16570427cc95c574d250fd899246e13
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-04-29 03:29:12 -0400

    Created skeleton code

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Endpoint working')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
