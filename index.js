1a980e5c04fb86723e03549bdcb6c408e7fd2565
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-05-22 22:14:41 -0400

    Deleted test endpoint

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Endpoint working')
-})
-
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
