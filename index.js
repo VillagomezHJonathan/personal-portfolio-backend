9bd848ac7fe6bb5b4feeacbe4d558965199e40f4
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-05-29 20:15:15 -0400

    Added endpoint path

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+app.post(`${process.env.ENDPOINT}`, (req, res) => {
+  res.send('hi')
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
